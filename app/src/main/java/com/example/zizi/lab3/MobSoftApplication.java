95ea566253ee4b601075416d7e342e1c60822108
Tankika <pt1029@gmail.com>
2017-03-24 10:05:58 +0100

    Dagger config

---
+++ app/src/main/java/com/example/zizi/lab3/MobSoftApplication.java
@@ -0,0 +1,21 @@
+package com.example.zizi.lab3;
+
+import android.app.Application;
+
+import com.example.zizi.lab3.ui.UIModule;
+
+public class MobSoftApplication extends Application {
+
+    public static MobSoftApplicationComponent injector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        injector =
+                DaggerMobSoftApplicationComponent.builder().
+                        uIModule(
+                                new UIModule(this)
+                        ).build();
+    }
+}
